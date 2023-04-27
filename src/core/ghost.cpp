f04d13506975432c5a586d53dc24477539b98f48
Arnaud <arnaud.filippi@etu.unistra.fr>
2023-04-28 00:43:11 +0200

    ghost show bon normalement ? je comprends pas la fin de la fonction mais bon je pense que c'est la même que pacman

--- src/core/ghost.cpp
+++ src/core/ghost.cpp
@@ -8,6 +8,27 @@ Ghost::Ghost(const double cx, const double cy, const double w, const double h,
     : Entity{cx, cy, w, h}, type{type} {}
 
 void Ghost::show(std::shared_ptr<Renderer> renderer) {
-  (void)renderer;
-  throw "Not implemented";
+
+  SDL_Rect asset;
+
+  switch (type) {
+  case GhostType::BLINKY:
+    asset = renderer->get_assets()->get_sprite_ghost_red(m_direction, 0);
+    break;
+  case GhostType::PINKY:
+    asset = renderer->get_assets()->get_sprite_ghost_pink(m_direction, 0);
+    break;
+  case GhostType::INKY:
+    asset = renderer->get_assets()->get_sprite_ghost_blue(m_direction, 0);
+    break;
+  case GhostType::CLYDE:
+    asset = renderer->get_assets()->get_sprite_ghost_orange(m_direction, 0);
+    break;
+  }
+
+  renderer->push();
+  renderer->rect_mode(RectMode::CENTER);
+  renderer->translate(m_cx, m_cy);
+  renderer->blit(asset, 0, 0);
+  renderer->pop();
 }
